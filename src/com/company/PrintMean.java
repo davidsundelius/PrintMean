f60b21b6e09650cae38dba054e0363d6deddae3c
David Sundelius <david.sundelius@gmail.com>
2020-09-10 11:45:57 +0200

    Initial commit

---
+++ src/com/company/PrintMean.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class PrintMean {
+    public static void main(String[] args) {
+        System.out.println("Mean of 1, 1, 3, 3: " + ((1 + 1 + 3 + 3) / 4));
+        System.out.println("Mean of 1, 2, 3, 4: " + ((1 + 2 + 3 + 4) / 4));
+        System.out.println("Mean of 1, -2, 3, -4: " + ((1 - 2 + 3 - 4) / 4));
+        System.out.println("Mean of -1, -11, 24, 2: " + ((-1 - 11 + 24 + 2) / 4));
+        System.out.println("Slut på program");
+    }
+}
