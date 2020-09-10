5b375ef920598ddc18a945f8d489c44e3ac822b2
David Sundelius <david.sundelius@gmail.com>
2020-09-10 13:25:38 +0200

    Added comment

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -2,6 +2,7 @@ package com.company;
 
 public class PrintMean {
     public static void main(String[] args) {
+        // Test
         System.out.println("Mean of 1, 1, 3, 3: " + ((1 + 1 + 3 + 3) / 4));
         System.out.println("Mean of 1, 2, 3, 4: " + ((1 + 2 + 3 + 4) / 4));
         System.out.println("Mean of 1, -2, 3, -4: " + ((1 - 2 + 3 - 4) / 4));
